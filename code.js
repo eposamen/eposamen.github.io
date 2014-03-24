8f9cd67660c055d4c26a890783d02c0dfa948873
eposamen <eposamen@gmail.com>
2014-03-24 14:59:12 -0400

    Simplified code

--- code.js
+++ code.js
@@ -6,17 +6,27 @@ var allStations;
 var allDistances;
 var jsonSched;
 var me = new google.maps.LatLng(myLat, myLng);
-var panToMark = new google.maps.LatLng(42.41342, -70.991648);
+var panLat = 42.3581;
+var panLng = -71.0636;
+var panToMark = new google.maps.LatLng(panLat, panLng);
 var myOptions = {
-			zoom: 13, 
+			zoom: 15, 
 			center: panToMark,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 var map;
 var marker;
 var places;
-
-
+var meMarker;
+var bluetxt = '[{"station":"Wonderland","lat":42.41342,"lng":-70.991648},{"station":"Revere Beach","lat":42.40784254,"lng":-70.99253321},{"station":"Beachmont","lat":42.39754234,"lng":-70.99231944},{"station":"Suffolk Downs","lat":42.39050067,"lng":-70.99712259},{"station":"Orient Heights","lat":42.386867,"lng":-71.00473599999999},{"station":"Wood Island","lat":42.3796403,"lng":-71.02286539000001},{"station":"Airport","lat":42.374262,"lng":-71.030395},{"station":"Maverick","lat":42.36911856,"lng":-71.03952958000001},{"station":"Aquarium","lat":42.359784,"lng":-71.051652},{"station":"State Street","lat":42.358978,"lng":-71.057598},{"station":"Government Center","lat":42.359705,"lng":-71.05921499999999},{"station":"Bowdoin","lat":42.361365,"lng":-71.062037}]';
+var orangetxt = '[{"station":"Oak Grove","lat":42.43668,"lng":-71.07109699999999},{"station":"Malden Center","lat":42.426632,"lng":-71.07411},{"station":"Wellington","lat":42.40237,"lng":-71.077082},{"station":"Sullivan","lat":42.383975,"lng":-71.076994},{"station":"Community College","lat":42.373622,"lng":-71.06953300000001},{"station":"North Station","lat":42.365577,"lng":-71.06129},{"station":"Haymarket","lat":42.363021,"lng":-71.05829},{"station":"State Street","lat":42.358978,"lng":-71.057598},{"station":"Downtown Crossing","lat":42.355518,"lng":-71.060225},{"station":"Chinatown","lat":42.352547,"lng":-71.062752},{"station":"Tufts Medical","lat":42.349662,"lng":-71.063917},{"station":"Back Bay","lat":42.34735,"lng":-71.075727},{"station":"Mass Ave","lat":42.341512,"lng":-71.083423},{"station":"Ruggles","lat":42.336377,"lng":-71.088961},{"station":"Roxbury Crossing","lat":42.331397,"lng":-71.095451},{"station":"Jackson Square","lat":42.323132,"lng":-71.099592},{"station":"Stony Brook","lat":42.317062,"lng":-71.104248},{"station":"Green Street","lat":42.310525,"lng":-71.10741400000001},{"station":"Forest Hills","lat":42.300523,"lng":-71.113686}]';
+var redtxt = '[{"station":"Alewife","lat":42.395428,"lng":-71.142483},{"station":"Davis","lat":42.39674,"lng":-71.121815},{"station":"Porter Square","lat":42.3884,"lng":-71.11914899999999},{"station":"Harvard Square","lat":42.373362,"lng":-71.118956},{"station":"Central Square","lat":42.365486,"lng":-71.103802},{"station":"Kendall/MIT","lat":42.36249079,"lng":-71.08617653},{"station":"Charles/MGH","lat":42.361166,"lng":-71.070628},{"station":"Park Street","lat":42.35639457,"lng":-71.0624242},{"station":"Downtown Crossing","lat":42.355518,"lng":-71.060225},{"station":"South Station","lat":42.352271,"lng":-71.05524200000001},{"station":"Broadway","lat":42.342622,"lng":-71.056967},{"station":"Andrew","lat":42.330154,"lng":-71.057655},{"station":"JFK/UMass","lat":42.320685,"lng":-71.052391},{"station":"North Quincy","lat":42.275275,"lng":-71.029583},{"station":"Wollaston","lat":42.2665139,"lng":-71.0203369},{"station":"Quincy Center","lat":42.251809,"lng":-71.005409},{"station":"Quincy Adams","lat":42.233391,"lng":-71.007153},{"station":"Braintree","lat":42.2078543,"lng":-71.0011385},{"station":"Savin Hill","lat":42.31129,"lng":-71.053331},{"station":"Fields Corner","lat":42.300093,"lng":-71.061667},{"station":"Shawmut","lat":42.29312583,"lng":-71.06573796000001},{"station":"Ashmont","lat":42.284652,"lng":-71.06448899999999}]';
+//var blueNumStations = 12;
+var blueInfo = JSON.parse('["color":"#0000FF", "numStops:":12, "icon":"markerblue.png"]');
+//var orangeNumStations = 19;
+var orangeInfo = JSON.parse('["color":"#ff9c00", "numStops:":19, "icon":"markerorange.png"]');
+//var redNumStations = 22;
+var redInfo = JSON.parse('["color":"#FF0000", "numStops:":22, "icon":"markerred.png"]');
 function init() {
 		req = new XMLHttpRequest();
 		req.open("get", "http://mbtamap.herokuapp.com/mapper/rodeo.json", true);
@@ -97,13 +107,80 @@ function renderMap()
 	panToMark = new google.maps.LatLng(42.41342, -70.991648);
 	map.panTo(panToMark);
 
-	var meMarker = new google.maps.Marker({
+	//var meMarker = new google.maps.Marker({
+	meMarker = new google.maps.Marker({
 		position: me,
 		title: "You are here"
 	});
 
 	meMarker.setMap(map);
 
+	if (jsonSched["line"] == "blue") {
+		createPath(blueInfo, bluetxt);
+	}
+
+	else if (jsonSched["line"] == "orange") {
+		createPath(orangeInfo, orangetxt);
+	}
+	else if (jsonSched["line"] == "red") {
+		var redtxt = '[{"station":"Alewife","lat":42.395428,"lng":-71.142483},{"station":"Davis","lat":42.39674,"lng":-71.121815},{"station":"Porter Square","lat":42.3884,"lng":-71.11914899999999},{"station":"Harvard Square","lat":42.373362,"lng":-71.118956},{"station":"Central Square","lat":42.365486,"lng":-71.103802},{"station":"Kendall/MIT","lat":42.36249079,"lng":-71.08617653},{"station":"Charles/MGH","lat":42.361166,"lng":-71.070628},{"station":"Park Street","lat":42.35639457,"lng":-71.0624242},{"station":"Downtown Crossing","lat":42.355518,"lng":-71.060225},{"station":"South Station","lat":42.352271,"lng":-71.05524200000001},{"station":"Broadway","lat":42.342622,"lng":-71.056967},{"station":"Andrew","lat":42.330154,"lng":-71.057655},{"station":"JFK/UMass","lat":42.320685,"lng":-71.052391},{"station":"North Quincy","lat":42.275275,"lng":-71.029583},{"station":"Wollaston","lat":42.2665139,"lng":-71.0203369},{"station":"Quincy Center","lat":42.251809,"lng":-71.005409},{"station":"Quincy Adams","lat":42.233391,"lng":-71.007153},{"station":"Braintree","lat":42.2078543,"lng":-71.0011385},{"station":"Savin Hill","lat":42.31129,"lng":-71.053331},{"station":"Fields Corner","lat":42.300093,"lng":-71.061667},{"station":"Shawmut","lat":42.29312583,"lng":-71.06573796000001},{"station":"Ashmont","lat":42.284652,"lng":-71.06448899999999}]';
+
+		var jsonRed= JSON.parse(redtxt);
+		var redLineStops1 = [];
+		var redLineStops2 = [];
+		for (var i = 0; i < 22; i++) {
+			var newLat = jsonRed[i]["lat"];
+			var newLng = jsonRed[i]["lng"];
+			var latlng = new google.maps.LatLng(newLat, newLng);
+			if (i == 12) {
+				
+				redLineStops2.push(latlng);
+			}
+			if (i < 18) {
+				redLineStops1.push(latlng);
+			}
+			else {
+				redLineStops2.push(latlng);
+			}
+
+			var stopMarker = new google.maps.Marker({
+				position: latlng,
+				map:map,
+				title: jsonRed[i]["station"],
+				icon: "markerred.png",
+			});
+			
+			stopMarker.setMap(map);
+
+			var infowindow = new google.maps.InfoWindow();  
+			google.maps.event.addListener(stopMarker, 'click', (function(stopMarker) {  
+				return function() {  
+					var content = writeStationInfo(stopMarker.title);
+					infowindow.setContent(content);  
+					infowindow.open(map, stopMarker);  
+				}  
+			})(stopMarker));
+
+		}
+
+		var redPath1 = new google.maps.Polyline({
+			path: redLineStops1,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 2
+		}); 
+		redPath1.setMap(map);
+
+		var redPath2 = new google.maps.Polyline({
+			path: redLineStops2,
+			strokeColor: '#FF0000',
+			strokeOpacity: 1.0,
+			strokeWeight: 2
+		}); 
+		redPath2.setMap(map);
+	}	
+	
+/*
 	if (jsonSched["line"] == "blue") {
 		var bluetxt = '[{"station":"Wonderland","lat":42.41342,"lng":-70.991648},{"station":"Revere Beach","lat":42.40784254,"lng":-70.99253321},{"station":"Beachmont","lat":42.39754234,"lng":-70.99231944},{"station":"Suffolk Downs","lat":42.39050067,"lng":-70.99712259},{"station":"Orient Heights","lat":42.386867,"lng":-71.00473599999999},{"station":"Wood Island","lat":42.3796403,"lng":-71.02286539000001},{"station":"Airport","lat":42.374262,"lng":-71.030395},{"station":"Maverick","lat":42.36911856,"lng":-71.03952958000001},{"station":"Aquarium","lat":42.359784,"lng":-71.051652},{"station":"State Street","lat":42.358978,"lng":-71.057598},{"station":"Government Center","lat":42.359705,"lng":-71.05921499999999},{"station":"Bowdoin","lat":42.361365,"lng":-71.062037}]';
 		var jsonBlue = JSON.parse(bluetxt);
@@ -185,78 +262,24 @@ function renderMap()
 		}); 
 		orangePath.setMap(map);
 	}
+	*/
 /////////////////////////////////// RED ///////////////////////////////////
-	else if (jsonSched["line"] == "red") {
-		var redtxt = '[{"station":"Alewife","lat":42.395428,"lng":-71.142483},{"station":"Davis","lat":42.39674,"lng":-71.121815},{"station":"Porter Square","lat":42.3884,"lng":-71.11914899999999},{"station":"Harvard Square","lat":42.373362,"lng":-71.118956},{"station":"Central Square","lat":42.365486,"lng":-71.103802},{"station":"Kendall/MIT","lat":42.36249079,"lng":-71.08617653},{"station":"Charles/MGH","lat":42.361166,"lng":-71.070628},{"station":"Park Street","lat":42.35639457,"lng":-71.0624242},{"station":"Downtown Crossing","lat":42.355518,"lng":-71.060225},{"station":"South Station","lat":42.352271,"lng":-71.05524200000001},{"station":"Broadway","lat":42.342622,"lng":-71.056967},{"station":"Andrew","lat":42.330154,"lng":-71.057655},{"station":"JFK/UMass","lat":42.320685,"lng":-71.052391},{"station":"North Quincy","lat":42.275275,"lng":-71.029583},{"station":"Wollaston","lat":42.2665139,"lng":-71.0203369},{"station":"Quincy Center","lat":42.251809,"lng":-71.005409},{"station":"Quincy Adams","lat":42.233391,"lng":-71.007153},{"station":"Braintree","lat":42.2078543,"lng":-71.0011385},{"station":"Savin Hill","lat":42.31129,"lng":-71.053331},{"station":"Fields Corner","lat":42.300093,"lng":-71.061667},{"station":"Shawmut","lat":42.29312583,"lng":-71.06573796000001},{"station":"Ashmont","lat":42.284652,"lng":-71.06448899999999}]';
 
-		var jsonRed= JSON.parse(redtxt);
-		var redLineStops1 = [];
-		var redLineStops2 = [];
-		for (var i = 0; i < 22; i++) {
-			var newLat = jsonRed[i]["lat"];
-			var newLng = jsonRed[i]["lng"];
-			var latlng = new google.maps.LatLng(newLat, newLng);
-			if (i == 12) {
-				
-				redLineStops2.push(latlng);
-			}
-			if (i < 18) {
-				redLineStops1.push(latlng);
-			}
-			else {
-				redLineStops2.push(latlng);
-			}
-
-			var stopMarker = new google.maps.Marker({
-				position: latlng,
-				map:map,
-				title: jsonRed[i]["station"],
-				icon: "markerred.png",
-			});
-			
-			stopMarker.setMap(map);
-
-			var infowindow = new google.maps.InfoWindow();  
-			google.maps.event.addListener(stopMarker, 'click', (function(stopMarker) {  
-				return function() {  
-					var content = writeStationInfo(stopMarker.title);
-					infowindow.setContent(content);  
-					infowindow.open(map, stopMarker);  
-				}  
-			})(stopMarker));
-
-		}
-
-		var redPath1 = new google.maps.Polyline({
-			path: redLineStops1,
-			strokeColor: '#FF0000',
-			strokeOpacity: 1.0,
-			strokeWeight: 2
-		}); 
-		redPath1.setMap(map);
-
-		var redPath2 = new google.maps.Polyline({
-			path: redLineStops2,
-			strokeColor: '#FF0000',
-			strokeOpacity: 1.0,
-			strokeWeight: 2
-		}); 
-		redPath2.setMap(map);
-	}
 
 
 
 
 	google.maps.event.addListener(meMarker, 'click', function() {
 		if (jsonSched["line"] == "blue") {
-			var content = writeMeInfo(meMarker, jsonBlue);
+
+			var content = writeMeInfo(meMarker, jsonInfo);
 		}
 		else if (jsonSched["line"] == "red") {
-			var content = writeMeInfo(meMarker, jsonRed);
+			var content = writeMeInfo(meMarker, jsonInfo);
 		}
 
 		else if (jsonSched["line"] == "orange") {
-			var content = writeMeInfo(meMarker, jsonOrange);
+			var content = writeMeInfo(meMarker, jsonInfo);
 		}
 		infowindow.setContent(content);
 		infowindow.open(map, meMarker);
@@ -445,3 +468,48 @@ function getTime(givenSeconds)
 	return finalTime;
 
 }
+
+
+
+function createPath(lineInfo, lineText) {
+	//var bluetxt = '[{"station":"Wonderland","lat":42.41342,"lng":-70.991648},{"station":"Revere Beach","lat":42.40784254,"lng":-70.99253321},{"station":"Beachmont","lat":42.39754234,"lng":-70.99231944},{"station":"Suffolk Downs","lat":42.39050067,"lng":-70.99712259},{"station":"Orient Heights","lat":42.386867,"lng":-71.00473599999999},{"station":"Wood Island","lat":42.3796403,"lng":-71.02286539000001},{"station":"Airport","lat":42.374262,"lng":-71.030395},{"station":"Maverick","lat":42.36911856,"lng":-71.03952958000001},{"station":"Aquarium","lat":42.359784,"lng":-71.051652},{"station":"State Street","lat":42.358978,"lng":-71.057598},{"station":"Government Center","lat":42.359705,"lng":-71.05921499999999},{"station":"Bowdoin","lat":42.361365,"lng":-71.062037}]';
+	
+	var jsonInfo = JSON.parse(lineText);
+	var lineStops = [];
+	for (station in jsonInfo)
+		//var newLat = jsonBlue[i]["lat"];
+		var newLat = station["lat"];
+		//var newLng = jsonBlue[i]["lng"];
+		var newLng = station["lng"];
+		var latlng = new google.maps.LatLng(newLat, newLng);
+		lineStops.push(latlng);
+		var stopMarker = new google.maps.Marker({
+			position: latlng,
+			map:map,
+			title: station["station"],
+			icon: lineInfo["icon"]
+		});
+		
+		stopMarker.setMap(map);
+		
+		var infowindow = new google.maps.InfoWindow();  
+		google.maps.event.addListener(stopMarker, 'click', (function(stopMarker) {  
+			return function() {  
+				var content = writeStationInfo(stopMarker.title);
+				infowindow.setContent(content);
+				infowindow.open(map, stopMarker);  
+			}
+		})(stopMarker));
+
+	}
+	var linePath = new google.maps.Polyline({
+		path: lineStops,
+		strokeColor: lineInfo["color"],
+		strokeOpacity: 1.0,
+		strokeWeight: 2
+	}); 
+	linePath.setMap(map);
+}
+
+
+
